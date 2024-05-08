4003283a8ffa9078cb0256cfade98cecebffceae
Jason Amey <jason.amey@gmail.com>
2024-05-08 11:54:34 -0400

    test html

---
+++ html-gen/index.py
@@ -0,0 +1,4 @@
+html_base = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> <h1>dognose</h1> </head><body></body></html>'
+
+with open("../index.html", "w") as fh:
+    fh.write(html_base)
