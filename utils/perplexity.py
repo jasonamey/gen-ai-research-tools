08fd7e47a22b06f3ce4ea91541c09f73c742fa48
Jason Amey <jason.amey@gmail.com>
2024-08-15 09:29:41 -0400

    perplexity change

--- utils/perplexity.py
+++ utils/perplexity.py
@@ -26,12 +26,13 @@ def get_perplexity_query(question):
         messages=create_message(question),
     )
     answer = {}
+    print("here is response:", response)
     answer["answer"] = response.choices[0].message.content
     return answer
 
 
-# QUESTION = "What are the major risks associated with international investments?"
+QUESTION = "can you find me scholarly journal articles on the question of: What impact did the Silk Road have on cultural exchange between East and West Asia?"
 
-# print(get_perplexity_query(QUESTION))
+print(get_perplexity_query(QUESTION))
 
 # {'answer': "Hello there! I'd be happy to help answer your question. When it comes to international investments, there are indeed some risks that are important to be aware of. Here are some of the major risks:\n\n1. Political Instability: Political instability in a foreign country can lead to economic instability, which can negatively impact your investments. This can include things like government instability, civil unrest, or changes in regulations that can make it difficult for foreign investors to operate.\n2. Currency Risk: When you invest in a foreign country, you are exposing yourself to the risk of currency fluctuations. If the value of the currency in which your investment is denominated declines relative to your home currency, the value of your investment may decrease as well.\n3. Economic Instability: Economic instability in a foreign country can also negatively impact your investments. This can include things like high inflation rates, high levels of debt, or a weak economy.\n4. Legal and Regulatory Risks: Differences in laws and regulations between your home country and the foreign country where you are investing can create risks. This can include things like different accounting standards, different tax laws, or different securities regulations.\n5. Cultural Differences: Cultural differences can also pose risks when it comes to international investments. For example, there may be differences in business practices, communication styles, or attitudes towards risk that can make it difficult to navigate the investment environment.\n\nI hope this information is helpful! If you have any further questions, please don't hesitate to ask. I'm here to help."}
